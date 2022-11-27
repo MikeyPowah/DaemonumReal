0d5c2232d98bcee8436e80987af1d337bc8f5b04
Apichu1836 <dpinesp@inf.upv.es>
2022-11-27 16:46:40 +0100

    Level Generation

--- Daemonum/Assets/Scripts/Movimiento.cs
+++ Daemonum/Assets/Scripts/Movimiento.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Movimiento : MonoBehaviour
 {
-    
+
     public Rigidbody rigidbody;
     public float moveSpeed;
     [SerializeField] private InteractionPromptUI _interactionPromptUI;
@@ -12,7 +12,7 @@ public class Movimiento : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
         rigidbody = GetComponent<Rigidbody>();
     }
 
@@ -24,13 +24,13 @@ public class Movimiento : MonoBehaviour
             if (!_interactionPromptUI.IsDisplayed) _interactionPromptUI.Open();
             else _interactionPromptUI.Close();
         }else if (!_interactionPromptUI.IsDisplayed){
-            moveInput.x = -Input.GetAxisRaw("Horizontal");
-            moveInput.y = -Input.GetAxisRaw("Vertical");
+            moveInput.x = Input.GetAxisRaw("Horizontal");
+            moveInput.y = Input.GetAxisRaw("Vertical");
             moveInput.Normalize();
-            
+
             rigidbody.velocity = new Vector3(moveInput.x * moveSpeed, rigidbody.velocity.y, moveInput.y * moveSpeed);
         }
-        
+
 
     }
 }
