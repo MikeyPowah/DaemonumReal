747c607561c30560b818a94d9e3d1c8c877b7f66
diegogs8 <101519632+diegogs8@users.noreply.github.com>
2022-11-29 20:41:22 +0100

    Interfaz gato y recibir daño

--- Daemonum/Assets/Scripts/Interacciones/StatsLorey.cs
+++ Daemonum/Assets/Scripts/Interacciones/StatsLorey.cs
@@ -7,7 +7,14 @@ public class Pantallazo : MonoBehaviour
 {
 
     [SerializeField] private GameObject  m_GotHitScreen;
-    private float vida = 10;
+    [SerializeField]
+    private GameObject player;
+    private StatsPlayer statsPlayer;
+
+    void Start()
+    {
+        statsPlayer = player.GetComponent<StatsPlayer>();
+    }
 
     public void gotHurt(float dmg)
     {
@@ -16,6 +23,7 @@ public class Pantallazo : MonoBehaviour
 
         m_GotHitScreen.GetComponent<Image>().color = color;
         Debug.Log(dmg);
+        statsPlayer.UpdateHealth(-1);
     }
 
     private void Update()
@@ -29,9 +37,9 @@ public class Pantallazo : MonoBehaviour
                 m_GotHitScreen.GetComponent<Image>().color = color;
             }
         }
-        if(vida <= 0)
+        if(statsPlayer.currentLife <= 0)
         {
-            
+            Debug.Log("muelto");
         }
     }
     
